fa7e5b0c65bdccc7fd1aa75b52efa0b4b97a5ea6
danielDelgado123 <delgadodaniel413@gmail.com>
2020-11-08 20:23:32 -0400

    addComitTodo lO Q ECHO

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Index from './src/component/index';
+import { Table, Row, Rows } from 'react-native-table-component';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>Indice de Masa Corporal</Text>
       <StatusBar style="auto" />
+      <Index/>
     </View>
   );
 }
@@ -18,4 +21,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginBottom: 40
+  }
 });
---
+++ src/component/index.js
@@ -0,0 +1,86 @@
+import React, {useState} from 'react';
+import {View,StyleSheet, Text, TextInput, Button} from 'react-native';
+
+export default function Index() {
+    const [ImcResultado, setImcResultado] = useState('');
+    const [ImcResultadoNum, setImcResultadoNum] = useState('');
+    const [formDato, setFormDato] = useState({});
+    const  iniciar = () =>{
+        if(!formDato.Peso || !formDato.Estatura){
+            console.log("llenar los datos");
+
+        } else {
+            // errores.correo = true;
+            console.log("daniel2");
+            let pesoAux = formDato.Peso;
+            let estaturaAux = formDato.Estatura;
+            let imc = pesoAux/((estaturaAux)^2);
+            console.log(imc);
+            if (imc < 18.5) { 
+                setImcResultado('Bajo peso');
+            }
+            if (imc <= 24.9 && imc >= 18.5) {
+                setImcResultado('Normal');
+            }
+            if (imc >= 25.0 && imc <= 29.9) {
+                setImcResultado('Sobrepeso');
+            }
+            if (imc >= 30.0) {
+                setImcResultado('Obeso');
+            }
+            setImcResultadoNum(imc);
+        }
+    }
+    const onChange = (e, tipo)=>{
+        setFormDato({...formDato, [tipo]: e.nativeEvent.text});
+    }
+    return (
+        <View>
+            <View style={styles.TextInput}>
+                <Text>Ingrese Peso:<Text style={styles.RedText}>(kg)</Text></Text>
+                <TextInput      
+                    style={{ borderColor: 'gray', borderWidth: 1, width: 200 }}
+                    placeholder="60.5" 
+                    placeholderTextColor="#969696"
+                    keyboardType={'numeric'}
+                    onChange= {(e) => onChange(e, "Peso")}/>
+            </View>
+            <View style={styles.TextInput}>
+                <Text>Ingrese Estatura:<Text style={styles.RedText}>(m)</Text></Text>
+                <TextInput  
+                    style={{ borderColor: 'gray', borderWidth: 1, width: 200 }}
+                    placeholder="1.70" 
+                    placeholderTextColor="#969696"
+                    keyboardType={'numeric'}
+                    onChange= {(e) => onChange(e, "Estatura")}/>
+            </View>
+            <Button title="Calcular IMC" onPress={iniciar}/>
+            <View style={styles.Resul}>
+                <Text  style={styles.titleText}>Nivel de Peso:</Text>
+                <Text style={styles.GreenText}>{ImcResultado ? ImcResultado : null}</Text>
+                <Text  style={styles.titleText}>IMC:</Text>
+                <Text>{ImcResultadoNum ? ImcResultadoNum : null}</Text>
+            </View>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    titleText: {
+      fontSize: 20,
+      fontWeight: "bold"
+    },
+    RedText: {
+        color: 'red'
+    },
+    GreenText: {
+        color: 'blue',
+        fontSize: 20,
+        fontWeight: "bold"
+    },
+    TextInput: {
+        marginBottom: 10
+    },
+    Resul: {
+        marginTop: 30
+    }
+});
